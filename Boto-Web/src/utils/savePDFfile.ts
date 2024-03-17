31ba77a352438b07af1d17d6a7af96c1ac9685bf
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-03-17 16:59:23 -0400

    add: save file function

--- Boto-Web/src/utils/savePDFfile.ts
+++ Boto-Web/src/utils/savePDFfile.ts
@@ -1,24 +1,8 @@
-import multer from 'multer';
-import { v4 as uuidv4 } from 'uuid';
-
 import FileSaver from 'file-saver';
 
-const storage = multer.diskStorage({
-  destination: function (request, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (request, file, cb) {
-    const extensaoArquivo = file.originalname.split('.')[1];
-
-    cb(null, `${uuidv4()}.pdf`);
-  },
-});
-
-const upload = multer({ storage });
-
-export function savePDFFile(pdf: any) {
-  console.log('oi');
-
-  // let blob = new Blob(['Hello, world!'], { type: 'text/plain;charset=utf-8' });
-  // FileSaver.saveAs(blob, 'hello world.txt');
+export function savePDFFile(nome: string, pdf: any) {
+  let blob = new Blob([pdf], { type: 'application/pdf' });
+  FileSaver.saveAs(blob, nome);
 }
+
+// savePDFFile(this.nome.value, this.file)
